ccb94177081c0bea22aae4bc94966c2203edf6db
Rogga22 <ra1644@nyu.edu>
2017-04-27 10:30:12 -0400

    Playercontrols/physics

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Rigidbody2D RB;
+    public float movespd;
+    public float jmp;
+    bool onfloor;
+
+    // Use this for initialization
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = RB.velocity;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (move.x > 0)
+                move.x = 0;
+            move.x = Mathf.Lerp(move.x, -movespd, 0.1f);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (move.x < 0)
+                move.x = 0;
+            move.x = Mathf.Lerp(move.x, movespd, 0.1f);
+        }
+        else
+            move.x = Mathf.Lerp(move.x, 0, 0.1f);
+        if (Input.GetKeyDown(KeyCode.Z) && onfloor == true)
+        {
+            move.y = jmp;
+            onfloor = false;
+        }
+        RB.velocity = move;
+    }
+
+    void OnCollisionEnter2D (Collision2D other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {
+            onfloor = true;
+        }
+    }
 }
+
+
